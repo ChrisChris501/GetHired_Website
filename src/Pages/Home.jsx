58f6b3523be2af5d95399c2be45daf319658e64d
ChrisChris501 <Christanahagbekeye@gmail.com>
2024-07-05 18:08:38 +0100

    Upgrading to a React app

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,15 @@
-<!doctype html>
-<html>
+import React from 'react'
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Latest FontAwesome CDN link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+const index = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-    <!-- Tailwind CSS -->
-    <link href="./output.css" rel="stylesheet">
-</head>
+export default index
+
 
 <body class="bg-red-100">
     <nav class="flex justify-between items-center p-4 bg-gradient-to-r from-red-700 via-orange-500  to-orange-800 shadow">
